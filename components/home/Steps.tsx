77677147c845f60c10056e0603815e1e6513e91f
Yuvraj Singh <mr.uvsingh@gmail.com>
2023-06-21 12:54:37 +0530

    minor change for nav bar z-index and steps section row-direction

--- components/home/Steps.tsx
+++ components/home/Steps.tsx
@@ -60,7 +60,7 @@ const Steps = () => {
             </div>
           </div>
           <div className="text-center h-[36vw]">
-            <video autoPlay muted loop src="/Step.mp4" className="h-full w-auto max-w-none rounded-3xl" />
+            <video autoPlay muted loop src="/Step.mp4" className="h-full inline w-auto max-w-none rounded-3xl" />
           </div>
           </div>
         </SwiperSlide>
@@ -83,7 +83,7 @@ const Steps = () => {
             </div>
           </div>
           <div className="text-center h-[36vw]">
-            <video  autoPlay muted loop src="/Step.mp4" className="h-full w-auto max-w-none rounded-3xl" />
+            <video  autoPlay muted loop src="/Step.mp4" className="h-full inline w-auto max-w-none rounded-3xl" />
           </div>
           </div>
         </SwiperSlide>
